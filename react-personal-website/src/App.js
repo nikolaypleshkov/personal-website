364a88b52811e3e249cb66f1ea2f254fda8cef61
nikolaypleshkov <icelol@abv.bg>
2021-03-28 13:59:02 +0300

    sidebar

--- react-personal-website/src/App.js
+++ react-personal-website/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router className="App">
+      <Sidebar />
       <Navbar />
     </Router>
   );
