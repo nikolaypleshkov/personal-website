60bb374cea98ec124c5d0609d0d1dc6864ccf49b
nikolaypleshkov <icelol@abv.bg>
2021-03-28 22:43:40 +0300

    Update index.js

--- react-personal-website/src/components/AboutSection/index.js
+++ react-personal-website/src/components/AboutSection/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function AboutSection() {
     return (
-        <div>
+        <>
+        <AboutSection>
             
-        </div>
+        </AboutSection>
+        </>
     )
 }
 
