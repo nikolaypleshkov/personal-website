358556e8d2be08b2a51cb66e5b253cd843493662
nikolaypleshkov <icelol@abv.bg>
2021-03-29 09:52:51 +0300

    Update data.js

---
+++ react-personal-website/src/components/AboutSection/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'TopLine title',
+    headline: 'Some headling....',
+    description: 'description....',
+    buttonLabel: 'Contact',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
