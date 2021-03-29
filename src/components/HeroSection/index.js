364e17035c75fbf9b88af19f4e87abb8421d6968
nikolaypleshkov <icelol@abv.bg>
2021-03-29 13:21:04 +0300

    update package

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './hero-elements'
-import Video from '../../video/Pexels Videos 2814773.mp4'
+import VideoBG from '../../video/Pexels Videos 2814773.mp4'
+import Video from 'react-native-video'
+import styled from "styled-components/native";
+
 import {Button} from '../Button'
 function HeroSection() {
     const [hover, setHover] = useState(false)
@@ -8,11 +11,30 @@ function HeroSection() {
     const onHover = () => {
         setHover(!hover)
     }
+    
+    const styles = StyleSheet.create({
+        backgroundVideo: {
+            postion: absolute,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            width: 100,
+            height: 100,
+            overflow: hidden
+        }
+      });
 
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoPlay="autoplay" loop="loop" muted src={Video} type='video/mp4' />
+                <Video muted={true}
+                       style={styles.backgroundVideo}
+                       repeat={true}
+                       resizeMode={"cover"}
+                       rate={1.0}
+                       ignoreSilentSwitch={"obey"} 
+                       src={VideoBG} type='video/mp4' />
             </HeroBg>
             <HeroContent>
                 <HeroH1>Nikolay Pleshkov | Personal website</HeroH1>
@@ -31,6 +53,7 @@ function HeroSection() {
             </HeroContent>
         </HeroContainer>
     )
+    
 }
 export default HeroSection
     
