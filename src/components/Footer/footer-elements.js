56f3af37519c41e58b42a29f4ea69b64bef923d5
nikolaypleshkov <icelol@abv.bg>
2021-03-31 17:24:12 +0300

    Update footer-elements.js

--- src/components/Footer/footer-elements.js
+++ src/components/Footer/footer-elements.js
@@ -50,56 +50,6 @@ export const FooterLinksItems = styled.div`
   }
 `
 
-export const FooterLinkTitle = styled.h1`
- font-size: 14px;
- margin-bottom: 16px;
-`
-
-export const FooterLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  margin-bottom: 0.5rem;
-  font-size: 14px;
-
-  &:hover{
-      color: #F50057;
-      transition: 0.3s ease-out;
-  }
-`
-
-export const SocialMedia = styled.section`
-  max-width: 1000px;
-  width: 100%;
-`
-
-export const SocialMediaWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 1100px;
-  margin: 40px auto 0 auto;
-
-  @media screen and (max-width: 820px){
-    flex-direction: column;
-  }
-`
-
-export const SocialMediaLogo = styled(Link)`
-  color: #fff;
-  cursor: pointer;
-  text-decoration: none;
-  font-size: 1.5rem;
-  display: flex;
-  align-items: center;
-  margin-bottom: 16px;
-  font-weight: bold;
-`
-
-export const WebsiteRights = styled.small`
-  color: #fff;
-  margin-bottom: 16px;
-`
-
 export const SocialIcons = styled.div`
   display: flex;
   justify-content: center;
