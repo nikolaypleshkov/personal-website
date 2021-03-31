4b03f418f8413add312b5f094a1a370154df7497
nikolaypleshkov <icelol@abv.bg>
2021-03-31 13:29:50 +0300

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -19,8 +19,7 @@ function Projects() {
                     <ProjectsIcon src={Icon1} />
                     <ProjectsH2>Reservation System Web</ProjectsH2>
                     <ProjectsP>Reservation System writed with Java using Servlet framework and MySQL database.
-                               You can register and login to the system. You can book your ticket using the data table,
-                               where displays avalible tickets for your search properties.</ProjectsP>    
+                               You can register and login to the system. You can search flights and book if thicket is avalible.</ProjectsP>    
                 </ProjectsCard>    
                 <ProjectsCard> 
                     <ProjectsIcon src={Icon2} />
