e04a7031fbe37282b5be5f564cffb7d69eeec0be
nikolaypleshkov <icelol@abv.bg>
2021-04-01 15:28:06 +0300

    Update data.js

---
+++ src/components/SkillsSection/data.js
@@ -0,0 +1,23 @@
+export const skillsSection = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Skills',
+    headline1: 'Programing Languages',
+    description1: 'Java',
+    description2: 'C#',
+    description3: 'C++',
+    headline2: 'Programing Languages',
+    description4: 'HTML+CSS',
+    description5: 'JavaScript (React + Next.js)',
+    description6: 'Bootstrap, Foundation, MDBootstrap',
+    buttonLabel: 'Projects',
+    imgStart: false,
+    img: require('../../images/undraw_percentages_0rur.svg').default,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+
+}
