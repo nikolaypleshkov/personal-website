a9d983e385214d58825943e098b358925eacdbdd
nikolaypleshkov <icelol@abv.bg>
2021-04-01 15:22:54 +0300

    update data.js

--- src/components/AboutSection/data.js
+++ src/components/AboutSection/data.js
@@ -1,4 +1,4 @@
-export const homeObjOne = {
+export const aboutSection = {
     id: 'about',
     lightBg: false,
     lightText: true,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import AboutSection from '../components/AboutSection'
-import { homeObjFour, homeObjOne, homeObjTwo, homeObjThree } from '../components/AboutSection/data'
+import { homeObjFour, aboutSection, homeObjTwo, homeObjThree } from '../components/AboutSection/data'
 import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
 import Navbar from '../components/Navbar'
@@ -22,7 +22,7 @@ const Home = () => {
            <Sidebar isOpen = {isOpen} toggle={toggle} />
            <Navbar toggle={toggle} /> 
            <HeroSection /> 
-           <AboutSection {...homeObjOne} />
+           <AboutSection {...aboutSection} />
            <Skills />
             <Projects />
            <AboutSection {...homeObjFour} />
