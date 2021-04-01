d19c0d7965f62e1e2dad3a115ab0158b4c975e17
nikolaypleshkov <icelol@abv.bg>
2021-04-01 15:10:23 +0300

    Skills Section

---
+++ src/components/SkillsSection/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function IndexSection() {
+    return (
+        <SkillsContainer>
+            <SkillsWrapper>
+                <Column1>
+                  <TextWrapper>
+                      <TopLine>Skills</TopLine>
+                      <Heading>Programing Languages</Heading>
+                      <Subtitle> - Language 1</Subtitle>
+                      <Subtitle> - Language 2</Subtitle>
+                      <Subtitle> - Language 3</Subtitle>
+                      <Heading>Web development</Heading>
+                      <Subtitle> HTLM/CSS</Subtitle>
+                      <Subtitle>JavaScript (React + Next.js)</Subtitle>
+                      <Subtitle>Bootstrap, Foundation, MDBoostrap</Subtitle>
+                  </TextWrapper>
+                </Column1>
+            </SkillsWrapper>
+        </SkillsContainer>
+    )
+}
+
+export default IndexSection
