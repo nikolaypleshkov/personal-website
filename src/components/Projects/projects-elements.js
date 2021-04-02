7ccba86707d3d9eeece64780b97eee3366b619b6
nikolaypleshkov <icelol@abv.bg>
2021-04-02 11:52:54 +0300

    Update projects-elements.js

--- src/components/Projects/projects-elements.js
+++ src/components/Projects/projects-elements.js
@@ -35,7 +35,7 @@ export const ProjectsWrapper = styled.div`
   }
 `
 
-export const ProjectsCard = styled.a`
+export const ProjectsCard = styled.div`
   background: #fff;
   display: flex;
   flex-direction: column;
