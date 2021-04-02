3e60fef99bf18f3f111cf7f4e8fcd0bdc0bf198c
nikolaypleshkov <icelol@abv.bg>
2021-04-02 11:32:50 +0300

    Update index.js

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Column1, TextWrapper, TopLine } from '../AboutSection/about-elements'
+import { Column1, Column2, Img, ImgWrap, TextWrapper, TopLine } from '../AboutSection/about-elements'
 import {ContactContainer, ContactWrapper, ContactRow} from './contact-elements'
 
-function Contact() {
+function Contact({id,lightBg,lightText,imgStart,topLine,darkText,img,alt,}) {
     return (
-        <ContactContainer>
+        <ContactContainer lightBg={lightBg} id={id}>
             <ContactWrapper>
-                <ContactRow>
+                <ContactRow imgStart={imgStart}>
                     <Column1>
                       <TextWrapper>
-                          <TopLine>Contact Me</TopLine>
+                          <TopLine>{topLine}</TopLine>
                                    
                       </TextWrapper>
                     </Column1>
+                    <Column2>
+                     <ImgWrap>
+                     <Img src={img} alt={img} />
+                     </ImgWrap>
+                    </Column2>
                 </ContactRow>
             </ContactWrapper>
         </ContactContainer>
