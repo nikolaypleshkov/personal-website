aa7cee48886da2272215e603feb08203b79672be
nikolaypleshkov <icelol@abv.bg>
2021-04-02 11:26:54 +0300

    Update index.js

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Column1, TextWrapper, TopLine } from '../AboutSection/about-elements'
+import {ContactContainer, ContactWrapper, ContactRow} from './contact-elements'
 
 function Contact() {
     return (
-        <div>
-            
-        </div>
+        <ContactContainer>
+            <ContactWrapper>
+                <ContactRow>
+                    <Column1>
+                      <TextWrapper>
+                          <TopLine>Contact Me</TopLine>
+                                   
+                      </TextWrapper>
+                    </Column1>
+                </ContactRow>
+            </ContactWrapper>
+        </ContactContainer>
     )
 }
 
