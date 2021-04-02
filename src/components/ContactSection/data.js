b49e5b8b185dcbef5570e263163e175489c55754
nikolaypleshkov <icelol@abv.bg>
2021-04-02 11:30:08 +0300

    Update data.js

---
+++ src/components/ContactSection/data.js
@@ -0,0 +1,15 @@
+
+export const contactContainer = {
+    id: 'contact',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'TopLine title',
+    imgStart: false,
+    img: require('../../images/undraw_contact_us_15o2.svg').default,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+
+}
