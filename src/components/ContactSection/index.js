06228c3d3e52de10bcd23c8d4d7f674daf95c33d
nikolaypleshkov <icelol@abv.bg>
2021-04-02 11:47:01 +0300

    Update index.js

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Column1, Column2, Img, ImgWrap, TextWrapper, TopLine } from '../AboutSection/about-elements'
 import {ContactContainer, ContactWrapper, ContactRow} from './contact-elements'
-
-function Contact({id,lightBg,lightText,imgStart,topLine,darkText,img,alt,}) {
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import InputGroup from 'react-bootstrap/InputGroup'
+function Contact({id,lightBg,imgStart,topLine,img,alt,}) {
     return (
         <ContactContainer lightBg={lightBg} id={id}>
             <ContactWrapper>
@@ -10,12 +13,41 @@ function Contact({id,lightBg,lightText,imgStart,topLine,darkText,img,alt,}) {
                     <Column1>
                       <TextWrapper>
                           <TopLine>{topLine}</TopLine>
-                                   
+                          <Form>
+                          <Form.Group controlId="formBasicEmail">
+                          <Form.Label>Email address</Form.Label>
+                          <Form.Control type="email" placeholder="Enter email" />
+                          </Form.Group>
+
+                          <Form.Group controlId="formBasicEmail">
+                          <Form.Label>Name</Form.Label>
+                          <Form.Control type="text" placeholder="Enter your name" />
+                          </Form.Group>
+
+                          <Form.Group controlId="formGridState">
+                          <Form.Label>State</Form.Label>
+                          <Form.Control as="select" defaultValue="Choose...">
+                          <option>No reason</option>
+                          <option>Just to say Hi!</option>
+                          <option>Business</option>
+                          <option>Other</option>
+                          </Form.Control>
+                          </Form.Group>
+
+                          <InputGroup>
+                          <InputGroup.Prepend>
+                          <InputGroup.Text>With textarea</InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <FormControl as="textarea" aria-label="With textarea" />
+                          </InputGroup>
+
+                          </Form>   
+
                       </TextWrapper>
                     </Column1>
                     <Column2>
                      <ImgWrap>
-                     <Img src={img} alt={img} />
+                     <Img src={img} alt={alt} />
                      </ImgWrap>
                     </Column2>
                 </ContactRow>
